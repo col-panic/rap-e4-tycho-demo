cd74bf865db8847481203cc7d902bcf7583c6c43
Marco Descher <marco@descher.at>
2021-03-30 10:29:08 +0200

    Update test.sh

--- at.colpanic.e4raptycho.releng/test.sh
+++ at.colpanic.e4raptycho.releng/test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#mvn clean verify
-mvn clean verify -o
+mvn clean verify
 rm -Rf test
 mkdir test
 cp ../at.colpanic.e4raptycho.product.feature/target/*.war test/
@@ -10,4 +9,4 @@ echo "BROWSE TO http://localhost:8080/e4raptycho-rap/e4 AFTER STARTUP"
 echo "Opens an OSGI console - use lb command to show bundles"
 echo "###############################################################"
 echo "###############################################################"
-docker run --rm -it -p 8080:8080 -p 8443:8443 -v $PWD/test/:/var/lib/jetty/webapps/ jetty:9-jre8
+docker run --rm -it -p 8080:8080 -p 8443:8443 -v $PWD/test/:/var/lib/jetty/webapps/ jetty:9-jre11
